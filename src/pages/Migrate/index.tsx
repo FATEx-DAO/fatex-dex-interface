8d5b3a4c913ca70df7bcdaa27bb1b618ef3c23ab
--unset <adamknuckey@gmail.com>
2021-08-22 10:49:15 -0400

    Added more of the base code for the liquidity migration page

--- src/pages/Migrate/index.tsx
+++ src/pages/Migrate/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 import styled, { ThemeContext } from 'styled-components/macro'
 import { Pair } from '@venomswap/sdk'
 
@@ -17,7 +17,7 @@ import { Dots } from '../../components/swap/styleds'
 import Web3Status from '../../components/Web3Status'
 
 const PageWrapper = styled(AutoColumn)`
-  max-width: 640px;
+  max-width: 500px;
   width: 100%;
 `
 
@@ -52,9 +52,87 @@ const WalletConnectWrapper = styled.div`
   margin: 10px auto;
 `
 
+const ViperswapLPPair = styled.div<{ selected: boolean }>`
+  display: block;
+  width: 100%;
+  border 3px solid ${({ theme, selected }) => (selected ? theme.text1 : theme.text3)};
+  border-radius: 10px;
+  padding: 20px 25px;
+  margin-bottom: 10px;
+  cursor: pointer;
+
+  :hover {
+    border: 3px solid ${({ theme }) => theme.text1};
+  }
+`
+
+const PairIcons = styled.div`
+  width: fit-content;
+  display: inline-block;
+  margin-right: 20px;
+`
+
+const PairName = styled.div`
+  width: fit-content;
+  display: inline-block;
+`
+
+const PairAmount = styled.div`
+  width: fit-content;
+  display: inline-block;
+  float: right;
+`
+
+const SubmitMigration = styled.div`
+  width: 100%;
+`
+
+const LPPairsWrapper = styled.div``
+
+const MigrationSummary = styled.div`
+  width: 100%;
+  display: block;
+  margin-bottom: 10px;
+`
+
+const MigrationLPName = styled.div`
+  display: inline-block;
+  vertical-align: top;
+`
+
+const MigrationAmount = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  float: right;
+`
+
+const MigrateButtonWrapper = styled.div``
+
+const MigrateButton = styled.div<{ disabled?: boolean }>`
+  text-align: center;
+  display: block;
+  width: 100%;
+  border 3px solid ${({ theme, disabled }) => (disabled ? theme.text3 : theme.text1)};
+  border-radius: 10px;
+  padding: 20px 25px;
+  margin-bottom: 10px;
+  cursor: ${({ disabled }) => (disabled ? 'auto' : 'pointer')};
+
+  :hover {
+    background-color: ${({ theme, disabled }) => (disabled ? 'none' : theme.text1)};
+    ${({ theme, disabled }) => !disabled && `color: ${theme.text6};`}
+  }
+  
+`
+
 export default function Pool() {
   const theme = useContext(ThemeContext)
   const { account } = useActiveWeb3React()
+
+  const [selectedLPPair, setSelectedLPPair] = useState<number | null>(null)
+  const selectedLPUnlocked = useState(false)[0]
+  const [unlocking, setUnlocking] = useState(false)
+  const [migrating, setMigrating] = useState(false)
 
   const trackedTokenPairs = useTrackedTokenPairs()
   const tokenPairsWithLiquidityTokens = useMemo(
@@ -82,6 +160,33 @@ export default function Pool() {
     fetchingV2PairBalances || v2Pairs?.length < liquidityTokensWithBalances.length || v2Pairs?.some(V2Pair => !V2Pair)
 
   const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair))
+
+  if (allV2PairsWithLiquidity) {
+    console.log('V2PAIRS LOADED') //TODO -remove
+  }
+
+  const userViperswapLPTokens = [
+    {
+      primary: 'ONE',
+      secondary: 'USDC',
+      amount: '420.69'
+    },
+    {
+      primary: 'ONE',
+      secondary: 'DAI',
+      amount: '69.420'
+    }
+  ]
+
+  const onUnlock = () => {
+    setUnlocking(true)
+    console.log('unlock')
+  }
+
+  const onMigrate = () => {
+    setMigrating(true)
+    console.log('migrate')
+  }
 
   return (
     <>
@@ -136,14 +241,57 @@ export default function Pool() {
                   <Dots>Loading</Dots>
                 </TYPE.body>
               </EmptyProposals>
-            ) : allV2PairsWithLiquidity?.length > 0 ? (
-              <>loaded</>
+            ) : /*allV2PairsWithLiquidity?.length > 0*/ userViperswapLPTokens.length > 0 ? (
+              <LPPairsWrapper>
+                {userViperswapLPTokens.map((pair, index) => (
+                  <ViperswapLPPair
+                    key={index}
+                    selected={selectedLPPair === index}
+                    onClick={() => setSelectedLPPair(index)}
+                  >
+                    <PairIcons>()()</PairIcons>
+                    <PairName>
+                      {pair.primary}-{pair.secondary}
+                    </PairName>
+                    <PairAmount>{pair.amount}</PairAmount>
+                  </ViperswapLPPair>
+                ))}
+              </LPPairsWrapper>
             ) : (
               <EmptyProposals>
                 <TYPE.body color={theme.text3} textAlign="center">
                   No liquidity found.
                 </TYPE.body>
               </EmptyProposals>
+            )}
+            {userViperswapLPTokens.length > 0 && selectedLPPair !== null && (
+              <SubmitMigration>
+                <MigrationSummary>
+                  <MigrationLPName>
+                    {userViperswapLPTokens[selectedLPPair].primary}-{userViperswapLPTokens[selectedLPPair].secondary}
+                  </MigrationLPName>
+                  <MigrationAmount>{userViperswapLPTokens[selectedLPPair].amount}</MigrationAmount>
+                </MigrationSummary>
+                <MigrateButtonWrapper>
+                  {selectedLPUnlocked ? (
+                    migrating ? (
+                      <MigrateButton disabled={true}>Migrating...</MigrateButton>
+                    ) : (
+                      <MigrateButton onClick={() => onMigrate()}>
+                        Migrate {userViperswapLPTokens[selectedLPPair].primary}-
+                        {userViperswapLPTokens[selectedLPPair].secondary}
+                      </MigrateButton>
+                    )
+                  ) : unlocking ? (
+                    <MigrateButton disabled={true}>Unlocking...</MigrateButton>
+                  ) : (
+                    <MigrateButton onClick={() => onUnlock()}>
+                      Unlock {userViperswapLPTokens[selectedLPPair].primary}-
+                      {userViperswapLPTokens[selectedLPPair].secondary}
+                    </MigrateButton>
+                  )}
+                </MigrateButtonWrapper>
+              </SubmitMigration>
             )}
           </AutoColumn>
         </AutoColumn>
