9c2079d099e22b1c8ac7a5f5ec959f14645210a1
Corey <corey@dolomite.io>
2022-05-25 20:23:16 -0400

    fixed url banner

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { AlertTriangle, X } from 'react-feather'
-import { useURLWarningToggle, useURLWarningVisible } from '../../state/user/hooks'
-import { isMobile } from 'react-device-detect'
 import { useActiveWeb3React } from '../../hooks'
 import { WEB_INTERFACES } from '../../constants'
+import { ExternalLink } from '../../theme'
 
 const PhishAlert = styled.div<{ isActive: any }>`
   width: 100%;
@@ -15,7 +14,7 @@ const PhishAlert = styled.div<{ isActive: any }>`
   font-size: 11px;
   justify-content: space-between;
   align-items: center;
-  height: 64px;
+  //height: 64px;
   display: ${({ isActive }) => (isActive ? 'flex' : 'none')};
 `
 
@@ -26,21 +25,27 @@ export const StyledClose = styled(X)`
 `
 
 export default function URLWarning() {
-  const { chainId } = useActiveWeb3React()
-  const webInterfaces = chainId && WEB_INTERFACES[chainId]
-  const defaultHostname = webInterfaces?.[0]
-  const currentHostname = window.location.hostname
-  const showURLWarning = currentHostname === 'old.app.fatex.io' || currentHostname === 'fatex-dao-old.web.app'
+  // const { chainId } = useActiveWeb3React()
+  // const webInterfaces = chainId && WEB_INTERFACES[chainId]
+  // const defaultHostname = webInterfaces?.[0]
+  // const currentHostname = window.location.hostname
+  // const showURLWarning = currentHostname === 'old.app.fatex.io' || currentHostname === 'fatex-dao-old.web.app'
+  const showURLWarning = true
 
   return (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
-        <AlertTriangle style={{ marginRight: 6 }} size={12} /> This is the old version of FATExDAO. If you are looking
-        for the current version of the site, visit{' '}
-        {/*<code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>{defaultHostname}</code>.*/}
-        <a href={'https://app.fatexfi.io'} style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>
-          {defaultHostname}
-        </a>
+        <ExternalLink
+          href={
+            'https://fatexdao.gitbook.io/fatexdao/fatexdao-dapps-and-tokens/harmony-greater-than-matic-reissue/fateh-holder-options'
+          }
+          style={{ fontWeight: 'bold' }}
+        >
+          <AlertTriangle style={{ marginRight: 6 }} size={12} />
+          FATExDAO is moving to MATIC & deploying FATExFi. Any member who owns $FATE rewarded here (on HARMONY ONE) has
+          2 options: an automatic airdrop, and/or to apply for a limited, &quot;fair,&quot; FATExFi pre-launch token
+          swap. Click on this box for details.
+        </ExternalLink>
       </div>
     </PhishAlert>
   )
